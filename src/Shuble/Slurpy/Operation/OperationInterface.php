6994af07ed8fbba906e4d390dae0b8c0a92f2699
dorian <baikunz@gmail.com>
2013-03-24 23:23:45 +0100

    Add OperationInterface

---
+++ src/Shuble/Slurpy/Operation/OperationInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Shuble\Slurpy\Operation;
+
+interface OperationInterface
+{
+    /**
+     * Get the operation's name
+     *
+     * @return string
+     */
+    public function getName();
+
+    /**
+     * Get the operation's arguments as an array of string
+     *
+     * @return array
+     */
+    public function getArguments();
+}
