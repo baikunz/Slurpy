ca46842a2f74befaad7689155073a83e239efde1
dorian <baikunz@gmail.com>
2013-03-27 23:09:32 +0100

    Add CatOperation support

---
+++ src/Shuble/Slurpy/Operation/CatOperation.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Shuble\Slurpy\Operation;
+
+use Shuble\Slurpy\Operation\OperationInterface;
+use Shuble\Slurpy\Operation\OperationArgument\PageRange;
+
+class CatOperation implements OperationInterface
+{
+    /**
+     * An array of page ranges as arguments
+     *
+     * @var PageRange[]
+     */
+    protected $pageRanges = array();
+
+    /**
+     * Sets page ranges
+     *
+     * @param PageRange[] $pageRanges An array of PageRange
+     */
+    public function setPageRanges(array $pageRanges)
+    {
+        $this->pageRanges = $pageRanges;
+
+        return $this;
+    }
+
+    /**
+     * Get page ranges
+     *
+     * @return PageRange[]
+     */
+    public function getPageRanges()
+    {
+        return $this->pageRanges;
+    }
+
+    /**
+     * Add a page range
+     *
+     * @param PageRange $pageRange
+     *
+     * @return OperationInterface
+     */
+    public function addPageRange(PageRange $pageRange)
+    {
+        $this->pageRanges[] = $pageRange;
+
+        return $this;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Shuble\Slurpy\Operation\OperationInterface::getName()
+     */
+    public function getName()
+    {
+        return 'cat';
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see \Shuble\Slurpy\Operation\OperationInterface::getArguments()
+     */
+    public function getArguments()
+    {
+        return array_map('strval', $this->pageRanges);
+    }
+}
---
+++ test/Shuble/Slurpy/Operation/CatOperationTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Shuble\Slurpy\Operation;
+
+use Shuble\Slurpy\Operation\CatOperation;
+
+class CatOperationTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGetSetPageRanges()
+    {
+        $catOperation = new CatOperation();
+        $this->assertEquals(
+            array(), $catOperation->getPageRanges(),
+            '->getPageRanges() returns an empty array after operation is contructed'
+        );
+
+        $pageRange = $this->getMock('Shuble\Slurpy\Operation\OperationArgument\PageRange', array(), array(), '', false);
+
+        $catOperation->setPageRanges(array($pageRange));
+        $this->assertEquals(
+            array($pageRange), $catOperation->getPageRanges(),
+            '->setPageRanges() sets specified page ranges'
+        );
+
+        $catOperation->setPageRanges(array($pageRange, $pageRange));
+        $this->assertEquals(
+            array($pageRange, $pageRange), $catOperation->getPageRanges(),
+            '->setPageRanges() override existing page ranges'
+        );
+    }
+
+    public function testAddPageRange()
+    {
+        $pageRange = $this->getMock('Shuble\Slurpy\Operation\OperationArgument\PageRange', array(), array(), '', false);
+
+        $catOperation = new CatOperation();
+        $catOperation->addPageRange($pageRange);
+
+        $this->assertEquals(
+            array($pageRange), $catOperation->getPageRanges(),
+            '->addPageRange() appends a page range to existing ones'
+        );
+    }
+
+    public function testGetName()
+    {
+        $catOperation = new CatOperation();
+
+        $this->assertEquals('cat', $catOperation->getName());
+    }
+
+    public function testGetArguments()
+    {
+        $pageRange = $this->getMock('Shuble\Slurpy\Operation\OperationArgument\PageRange', array('__toString'), array(), '', false);
+        $pageRange
+            ->expects($this->once())
+            ->method('__toString')
+            ->will($this->returnValue('arg1'))
+        ;
+
+        $catOperation = new CatOperation();
+        $catOperation->addPageRange($pageRange);
+
+        $this->assertEquals(array('arg1'), $catOperation->getArguments());
+    }
+}
