0dab1039cea708c5909ee68c6b6420b60531611b
dorian <baikunz@gmail.com>
2013-03-30 16:31:21 +0100

    Add generate method

--- src/Shuble/Slurpy/Slurpy.php
+++ src/Shuble/Slurpy/Slurpy.php
@@ -2,6 +2,7 @@
 
 namespace Shuble\Slurpy;
 
+use Symfony\Component\Process\Process;
 use Shuble\Slurpy\Operation\OperationInterface;
 
 /**
@@ -201,6 +202,138 @@ class Slurpy
         return $this->buildCommand($options);
     }
 
+    public function generate(array $options = array() , $overwrite = false)
+    {
+        if (null === $this->binary) {
+            throw new \LogicException('You must define the pdftk binary');
+        }
+
+        if (empty($this->inputs)) {
+            throw new \LogicException('You must define at least one input file');
+        }
+
+        if (null === $this->output) {
+            throw new \LogicException('You must define an output file');
+        }
+
+        $this->prepareOutput($output, $overwrite);
+
+        $command = $this->getCommand($options);
+
+        list($status, $stdout, $stderr) = $this->executeCommand($command);
+        $this->checkProcessStatus($status, $stdout, $stderr, $command);
+
+        // TODO: Deal with operations without output files...
+//         $this->checkOutput($output, $command);
+    }
+
+    /**
+     * Checks the specified output
+     *
+     * @param  string $output  The output filename
+     * @param  string $command The generation command
+     *
+     * @throws \RuntimeException if the output file generation failed
+     */
+    protected function checkOutput($output, $command)
+    {
+        // the output file must exist
+        if (!$this->fileExists($output)) {
+            throw new \RuntimeException(sprintf(
+                'The file \'%s\' was not created (command: %s).',
+                $output, $command
+            ));
+        }
+
+        // the output file must not be empty
+        if (0 === $this->filesize($output)) {
+            throw new \RuntimeException(sprintf(
+                'The file \'%s\' was created but is empty (command: %s).',
+                $output, $command
+            ));
+        }
+    }
+
+    /**
+     * Checks the process return status
+     *
+     * @param  int   $status    The exit status code
+     * @param  string $stdout   The stdout content
+     * @param  string $stderr   The stderr content
+     * @param  string $command  The run command
+     *
+     * @throws \RuntimeException if the output file generation failed
+     */
+    protected function checkProcessStatus($status, $stdout, $stderr, $command)
+    {
+        if (0 !== $status and '' !== $stderr) {
+            throw new \RuntimeException(sprintf(
+                'The exit status code \'%s\' says something went wrong:'."\n"
+                .'stderr: "%s"'."\n"
+                .'stdout: "%s"'."\n"
+                .'command: %s.',
+                $status, $stderr, $stdout, $command
+            ));
+        }
+    }
+
+    /**
+     * Executes the given command via shell and returns the complete output as
+     * a string
+     *
+     * @param  string $command
+     *
+     * @return array(status, stdout, stderr)
+     */
+    protected function executeCommand($command)
+    {
+        $process = new Process($command);
+
+        $process->run();
+
+        return array(
+            $process->getExitCode(),
+            $process->getOutput(),
+            $process->getErrorOutput(),
+        );
+    }
+
+    /**
+     * Prepares the specified output
+     *
+     * @param  string  $filename  The output filename
+     * @param  boolean $overwrite Whether to overwrite the file if it already
+     *                            exist
+     */
+    protected function prepareOutput($filename, $overwrite)
+    {
+        $directory = dirname($filename);
+
+        if ($this->fileExists($filename)) {
+            if (!$this->isFile($filename)) {
+                throw new \InvalidArgumentException(sprintf(
+                    'The output file \'%s\' already exists and it is a %s.',
+                    $filename, $this->isDir($filename) ? 'directory' : 'link'
+                ));
+            } elseif (false === $overwrite) {
+                throw new \InvalidArgumentException(sprintf(
+                    'The output file \'%s\' already exists.',
+                    $filename
+                ));
+            } elseif (!$this->unlink($filename)) {
+                throw new \RuntimeException(sprintf(
+                    'Could not delete already existing output file \'%s\'.',
+                    $filename
+                ));
+            }
+        } elseif (!$this->isDir($directory) && !$this->mkdir($directory)) {
+            throw new \RuntimeException(sprintf(
+                'The output file\'s directory \'%s\' could not be created.',
+                $directory
+            ));
+        }
+    }
+
     /**
      * Sets all options
      */
@@ -347,5 +480,89 @@ class Slurpy
         }
 
         return $mergedOptions;
+    }
+
+    /**
+     * Wrapper for the "file_get_contents" function
+     *
+     * @param  string $filename
+     *
+     * @return string
+     */
+    protected function getFileContents($filename)
+    {
+        return file_get_contents($filename);
+    }
+
+    /**
+     * Wrapper for the "file_exists" function
+     *
+     * @param  string $filename
+     *
+     * @return boolean
+     */
+    protected function fileExists($filename)
+    {
+        return file_exists($filename);
+    }
+
+    /**
+     * Wrapper for the "is_file" method
+     *
+     * @param  string $filename
+     *
+     * @return boolean
+     */
+    protected function isFile($filename)
+    {
+        return is_file($filename);
+    }
+
+    /**
+     * Wrapper for the "filesize" function
+     *
+     * @param  string $filename
+     *
+     * @return integer or FALSE on failure
+     */
+    protected function filesize($filename)
+    {
+        return filesize($filename);
+    }
+
+    /**
+     * Wrapper for the "unlink" function
+     *
+     * @param  string $filename
+     *
+     * @return boolean
+     */
+    protected function unlink($filename)
+    {
+        return unlink($filename);
+    }
+
+    /**
+     * Wrapper for the "is_dir" function
+     *
+     * @param  string $filename
+     *
+     * @return boolean
+     */
+    protected function isDir($filename)
+    {
+        return is_dir($filename);
+    }
+
+    /**
+     * Wrapper for the mkdir function
+     *
+     * @param  string $pathname
+     *
+     * @return boolean
+     */
+    protected function mkdir($pathname)
+    {
+        return mkdir($pathname, 0777, true);
     }
 }
