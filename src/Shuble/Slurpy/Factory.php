75bb770e016fd3efe89bd5377e110d58ba69c523
dorian <baikunz@gmail.com>
2013-04-07 19:33:17 +0200

    Add missing operations to factory

--- src/Shuble/Slurpy/Factory.php
+++ src/Shuble/Slurpy/Factory.php
@@ -2,9 +2,27 @@
 
 namespace Shuble\Slurpy;
 
+use Shuble\Slurpy\Operation\AttachFilesOperation;
+
+use Shuble\Slurpy\Operation\UpdateInfoOperation;
+
+use Shuble\Slurpy\Operation\UnpackFilesOperation;
+
+use Shuble\Slurpy\Operation\DumpDataOperation;
+
+use Shuble\Slurpy\Operation\StampOperation;
+
+use Shuble\Slurpy\Operation\FillFormOperation;
+
+use Shuble\Slurpy\Operation\GenerateFdfOperation;
+
+use Shuble\Slurpy\Operation\BurstOperation;
+
+use Shuble\Slurpy\Operation\BackgroundOperation;
+use Shuble\Slurpy\Operation\ShuffleOperation;
+use Shuble\Slurpy\Operation\CatOperation;
 use Shuble\Slurpy\Operation\OperationArgument\PageRange;
 use Shuble\Slurpy\InputFile;
-use Shuble\Slurpy\Operation\CatOperation;
 
 class Factory
 {
@@ -47,47 +65,288 @@ class Factory
      *        ...
      *    )
      * @param string $output
+     *
+     * @return Slurpy
      */
     public function cat(array $inputs, $output)
+    {
+        $slurpy = $this->getSlurpy($output);
+
+        $operation = new CatOperation();
+        $slurpy->setOperation($operation);
+
+        foreach ($inputs as $index => $input) {
+            if (is_array($input) && empty($input['filepath'])) {
+                throw new \InvalidArgumentException('You must specify a filepath for each input files');
+            }
+
+            $inputFile = $this->getInputFile($input, $index);
+            $slurpy->addInput($inputFile);
+
+            $pageRange = $this->getPageRange($inputFile->getHandle(), $input);
+            $operation->addPageRange($pageRange);
+        }
+
+        return $slurpy;
+    }
+
+    public function shuffle(array $inputs, $output)
+    {
+        $slurpy = $this->getSlurpy($output);
+
+        $operation = new ShuffleOperation();
+        $slurpy->setOperation($operation);
+
+        foreach ($inputs as $index => $input) {
+            if (is_array($input) && empty($input['filepath'])) {
+                throw new \InvalidArgumentException('You must specify a filepath for each input files');
+            }
+
+            $inputFile = $this->getInputFile($input, $index);
+            $slurpy->addInput($inputFile);
+
+            $pageRange = $this->getPageRange($inputFile->getHandle(), $input);
+            $operation->addPageRange($pageRange);
+        }
+
+        return $slurpy;
+    }
+
+    public function background($input, $background, $output, $multi = false)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new BackgroundOperation();
+        $operation
+            ->setMulti($multi)
+            ->setBackgroundFile($background)
+        ;
+        $slurpy->setOperation($operation);
+
+        return $slurpy;
+    }
+
+    public function multiBackground($input, $background, $output)
+    {
+        return $this->background($input, $background, $output, true);
+    }
+
+    public function burst($input, $output)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        // TODO: deal with null output
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $slurpy->setOperation(new BurstOperation());
+
+        return $slurpy;
+    }
+
+    public function generateFdf($input, $output)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $slurpy->setOperation(new GenerateFdfOperation());
+
+        return $slurpy;
+    }
+
+    public function fillForm($input, $dataFile, $output, $flatten = true)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new FillFormOperation();
+        $operation->setDataFile($dataFile);
+        $slurpy->setOperation($operation);
+
+        $slurpy->setOption('flatten', $flatten);
+
+        return $slurpy;
+    }
+
+    public function stamp($input, $stampFile, $output, $multi = false)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new StampOperation();
+        $operation
+            ->setMulti($multi)
+            ->setStampFile($stampFile)
+        ;
+        $slurpy->setOperation($operation);
+
+        return $slurpy;
+    }
+
+    public function multistamp($input, $stampFile, $output)
+    {
+        return $this->stamp($input, $stampFile, $output, true);
+    }
+
+    public function dumpData($input, $output, $fields = false, $utf8 = false)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new DumpDataOperation();
+        $operation
+            ->setFields($fields)
+            ->setUtf8($utf8)
+        ;
+        $slurpy->setOperation($operation);
+
+        return $slurpy;
+    }
+
+    public function dumpDataFields($input, $output)
+    {
+        return $this->dumpData($input, $output, true);
+    }
+
+    public function dumpDataUtf8($input, $output)
+    {
+        return $this->dumpData($input, $output, false, true);
+    }
+
+    public function dumpDataFieldsUtf8($input, $output)
+    {
+        return $this->dumpData($input, $output, true, true);
+    }
+
+    public function unpackFiles($input, $output)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $slurpy->setOperation(new UnpackFilesOperation());
+
+        return $slurpy;
+    }
+
+    public function updateInfo($input, $dataFile, $output, $utf8 = false)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new UpdateInfoOperation();
+        $operation
+            ->setDataFile($dataFile)
+            ->setUtf8($utf8)
+        ;
+        $slurpy->setOperation($operation);
+
+        return $slurpy;
+    }
+
+    public function attachFiles($input, array $files, $output, $toPage = null)
+    {
+        if (is_array($input) && empty($input['filepath'])) {
+            throw new \InvalidArgumentException('You must specify a filepath for the input file');
+        }
+
+        $slurpy = $this->getSlurpy($output);
+
+        $inputFile = $this->getInputFile($input);
+        $slurpy->addInput($inputFile);
+
+        $operation = new AttachFilesOperation();
+        $operation
+            ->setFiles($files)
+            ->setToPage($toPage)
+        ;
+        $slurpy->setOperation($operation);
+
+        return $slurpy;
+    }
+
+    protected function getSlurpy($output)
     {
         $slurpy = new Slurpy($this->binary);
 
         $slurpy->setOutput($output);
 
-        $operation = new CatOperation();
-
-        foreach ($inputs as $index => $input) {
-            $inputFile = new InputFile();
-
-            if (is_array($input) && empty($input['filepath'])) {
-                throw new \InvalidArgumentException('You must specify a filepath for each input files');
-            }
-
-            $handle = $this->generateFileHandle($index);
-            $filePath = is_array($input) ? $input['filepath'] : $input;
-            $filePassword = is_array($input) && !empty($input['password'])
-                ? $input['password']
-                : null
-            ;
-
-            $inputFile->setHandle($handle);
-            $inputFile->setFilePath($filePath);
-            $inputFile->setPassword($filePassword);
-
-            $slurpy->addInput($inputFile);
-
-            $startPage = is_array($input) && !empty($input['start_page']) ? $input['start_page'] : null;
-            $endPage = is_array($input) && !empty($input['end_page']) ? $input['end_page'] : null;
-            $qualifier = is_array($input) && !empty($input['qualifier']) ? $input['qualifier'] : null;
-            $rotation = is_array($input) && !empty($input['rotation']) ? $input['rotation'] : null;
-
-            $pageRange = new PageRange($handle, $startPage, $endPage, $qualifier, $rotation);
-            $operation->addPageRange($pageRange);
-
-            $slurpy->setOperation($operation);
-        }
-
         return $slurpy;
+    }
+
+    protected function getInputFile($input, $index = 0)
+    {
+        $handle = $this->generateFileHandle($index);
+        $filePath = is_array($input) ? $input['filepath'] : $input;
+        $filePassword = is_array($input) && !empty($input['password'])
+            ? $input['password']
+            : null
+        ;
+
+        $inputFile = new InputFile();
+        $inputFile
+            ->setHandle($handle)
+            ->setFilePath($filePath)
+            ->setPassword($filePassword)
+        ;
+
+        return $inputFile;
+    }
+
+    protected function getPageRange($handle, $input)
+    {
+        $startPage = is_array($input) && !empty($input['start_page']) ? $input['start_page'] : null;
+        $endPage = is_array($input) && !empty($input['end_page']) ? $input['end_page'] : null;
+        $qualifier = is_array($input) && !empty($input['qualifier']) ? $input['qualifier'] : null;
+        $rotation = is_array($input) && !empty($input['rotation']) ? $input['rotation'] : null;
+
+        return new PageRange($handle, $startPage, $endPage, $qualifier, $rotation);
     }
 
     /**
