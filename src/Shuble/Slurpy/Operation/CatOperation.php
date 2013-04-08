aae83d561928f0075e9029717ec1b86e09f64d6c
dorian <baikunz@gmail.com>
2013-04-08 22:21:33 +0200

    Add docblocks for CatOperation

--- src/Shuble/Slurpy/Operation/CatOperation.php
+++ src/Shuble/Slurpy/Operation/CatOperation.php
@@ -1,10 +1,29 @@
 <?php
 
+/**
+ * This file is part of the Slurpy package.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @license MIT License
+ */
+
 namespace Shuble\Slurpy\Operation;
 
 use Shuble\Slurpy\Operation\OperationInterface;
 use Shuble\Slurpy\Operation\OperationArgument\PageRange;
 
+/**
+ * Assembles ("catenates") pages from input PDFs to create a new PDF.
+ * Use cat to merge PDF pages or to split PDF pages from documents.
+ * You can also use it to rotate PDF pages. Page order in the new PDF
+ * is specified by the order of the given page ranges.
+ *
+ * @see PageRange
+ * @link http://www.pdflabs.com/docs/pdftk-man-page/#dest-op-cat
+ *
+ * @author dorian ALKOUM <baikunz@gmail.com>
+ */
 class CatOperation implements OperationInterface
 {
     /**
@@ -16,6 +35,8 @@ class CatOperation implements OperationInterface
 
     /**
      * Sets page ranges
+     *
+     * @see PageRange
      *
      * @param PageRange[] $pageRanges An array of PageRange
      */
@@ -29,6 +50,8 @@ class CatOperation implements OperationInterface
     /**
      * Get page ranges
      *
+     * @see PageRange
+     *
      * @return PageRange[]
      */
     public function getPageRanges()
@@ -39,7 +62,9 @@ class CatOperation implements OperationInterface
     /**
      * Add a page range
      *
-     * @param PageRange $pageRange
+     * @see PageRange
+     *
+     * @param PageRange $pageRange A single PageRange
      *
      * @return OperationInterface
      */
