411647f2d7b596a98b15c8d6847d30242624f441
dorian <baikunz@gmail.com>
2013-04-10 19:36:22 +0200

    Update autoload

--- src/autoload.php
+++ src/autoload.php
@@ -8,15 +8,22 @@
  * @see https://wiki.php.net/rfc/splclassloader#example_implementation
  */
 spl_autoload_register(function($className) {
-    $package = 'Shuble\\Slurpy';
     $className = ltrim($className, '\\');
-    if (0 === strpos($className, $package)) {
-        $fileName = __DIR__ . DIRECTORY_SEPARATOR . str_replace('\\', DIRECTORY_SEPARATOR, $className) . '.php';
-        if (is_file($fileName)) {
-            require $fileName;
+    if (0 != strpos($className, 'Shuble\\Slurpy')) {
+        return false;
+    }
+    $fileName = '';
+    $namespace = '';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . $className . '.php';
+    if (is_file($fileName)) {
+        require $fileName;
 
-            return true;
-        }
+        return true;
     }
 
     return false;
